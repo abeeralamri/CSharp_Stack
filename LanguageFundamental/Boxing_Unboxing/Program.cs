0f61c4d0970b09868b59cf5c0e54c6c7299c4182
Abeer <Abeer.alamri1997@gmail.com>
2021-06-20 22:49:45 +0300

    msg

---
+++ LanguageFundamental/Boxing_Unboxing/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Boxing_Unboxing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Object> obj = new List<Object>();
+            obj.Add(7);
+            obj.Add(28);
+            obj.Add(-1);
+            obj.Add(true);
+            obj.Add("chair");
+            int sum = 0;
+            for(int i = 0; i<obj.Count; i++){
+                Console.WriteLine(obj[i]);
+                if(obj[i] is int){
+                    sum = sum + (int)obj[i] ;
+                    
+                }
+                
+            }
+            
+            Console.WriteLine("Sum:" + sum);
+
+        }
+    }
+}
