34cb6970fb4a4220e0351260a4f74dbad10a2e07
Abeer <Abeer.alamri1997@gmail.com>
2021-06-26 20:50:54 +0300

    DojoSurveyWithModel

---
+++ ASP2/DojoSurveyWithModel/Controllers/SurveyController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using DojoSurveyWithModel.Models;
+
+namespace DojoSurveyWithModel.Controllers
+{
+    public class SurveyController : Controller
+    {
+       [HttpGet]
+       [Route("")]
+
+       public IActionResult Index(){
+           return View();
+       }
+
+       [HttpPost]
+       [Route("result")]
+
+       public IActionResult Result(Survey survey){
+            
+           return View("Display", survey);
+
+       }
+    }
+}
---
+++ ASP2/DojoSurveyWithModel/Models/Survey.cs
@@ -0,0 +1,10 @@
+namespace DojoSurveyWithModel.Models
+{
+    public class Survey
+    {
+        public string Name {get;set;}
+        public string Loc {get;set;}
+        public string Lang {get;set;}
+        public string Comment {get;set;}
+    }
+}
